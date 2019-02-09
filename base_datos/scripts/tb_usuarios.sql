be27a5e3595b0802e3e0702018e3882ca6ca8a26
Angel Hernandez <angel_hernandez998@hotmail.com>
2019-02-08 22:41:35 -0600

    Se finalizaron las funciones y procedimientos almacenados de tb_usuario

--- base_datos/scripts/tb_usuarios.sql
+++ base_datos/scripts/tb_usuarios.sql
@@ -1,7 +1,79 @@
+
 /*
-drop procedure if exists `proc_nuevo_usuario`;
+drop function if exists func_get_id_usuario_by_email;
 delimiter $$
-create procedure proc_nuevo_usuario(in p_nombre varchar(200),
+create function func_get_id_usuario_by_email(p_email varchar(200))
+returns bigint
+begin
+	set @response_id = (select id 
+						from tb_usuario 
+						where correo = p_email and
+								eliminado = false);
+	return @response_id;
+end $$
+delimiter ;
+*/
+
+/*
+drop function if exists func_is_user_admin;
+delimiter $$
+create function func_is_user_admin(p_id bigint)
+returns boolean
+begin
+	set @r_admin = false;
+    if(valid_int_id(p_id)) then
+		set @r_admin = (select admin from tb_usuario
+						where id = p_id
+                        and eliminado = false);
+		if(@r_admin is null) then
+			set @r_admin = false;
+        end if;
+    end if;
+    return @r_admin;
+end $$
+delimiter ;
+*/
+
+/*
+drop procedure if exists `proc_get_usuarios`;
+delimiter $$
+create procedure proc_get_usuarios()
+begin
+	select u.id,
+			u.nombre,
+            u.nombre_usuario,
+            u.admin,
+            u.habilitado,
+            u.primera_sesion
+    from tb_usuario u where
+    u.eliminado = false
+    order by u.nombre asc;
+end $$
+delimiter ;
+*/
+
+/*
+drop procedure if exists `proc_get_usuario_by_email`;
+delimiter $$
+create procedure proc_get_usuario_by_email(in p_correo varchar(200))
+begin
+	set p_correo = trim_and_lower(p_correo);
+    select u.id,
+			u.nombre,
+            u.nombre_usuario,
+            u.admin,
+            u.habilitado,
+            u.primera_sesion
+    from tb_usuario u where
+    u.correo = p_correo;
+end $$
+delimiter ;
+*/
+
+/*
+drop procedure if exists `proc_add_usuario`;
+delimiter $$
+create procedure proc_add_usuario(in p_nombre varchar(200),
 									in p_nombre_usuario varchar(100),
 									in p_correo varchar(200),
                                     in p_clave varchar(255),
@@ -38,27 +110,116 @@ delimiter ;
 */
 
 /*
-drop procedure if exists `proc_borrar_usuario_by_id`;
+drop procedure if exists `proc_delete_usuario_by_id`;
 delimiter $$
-create procedure proc_borrar_usuario_by_id(in p_id bigint,
+create procedure proc_delete_usuario_by_id(in p_id bigint,
 											in p_id_usuario_eliminado_por bigint)
 begin
-	set @r_success = false;
-    
+
     if (valid_int_id(p_id) and 
 		valid_int_id(p_id_usuario_eliminado_por)) then
         UPDATE `db_rimeim`.`tb_usuario` SET
 		`id_usuario_eliminado_por` = p_id_usuario_eliminado_por,
 		`habilitado` = false,
-		`eliminado` = false,
+		`eliminado` = true,
 		`fecha_eliminado` = current_timestamp()
 		WHERE `id` = p_id;
 		
-        set @r_success = true;
     end if;
-    
-    select @r_success as 'success';
 end $$
 delimiter ;
 */
+
+/*
+drop procedure if exists `proc_disable_usuario_by_id`;
+delimiter $$
+create procedure proc_disable_usuario_by_id(in p_id_usuario bigint)
+begin
+	if(valid_int_id(p_id_usuario)) then
+		update tb_usuario
+        set habilitado = false
+        where id = p_id_usuario
+        and eliminado = false;
+    end if;
+end $$
+delimiter ;
+*/
+
+/*
+drop procedure if exists `proc_enable_usuario_by_id`;
+delimiter $$
+create procedure proc_enable_usuario_by_id(in p_id_usuario bigint)
+begin
+	if(valid_int_id(p_id_usuario)) then
+		update tb_usuario
+        set habilitado = true
+        where id = p_id_usuario
+        and eliminado = false;
+    end if;
+end $$
+delimiter ;
+*/
+
+/*
+drop procedure if exists `proc_update_usuario_by_id`;
+delimiter $$
+create procedure proc_update_usuario_by_id(in p_id_usuario bigint,
+											in p_nombre varchar(200),
+                                            in p_nombre_usuario varchar(100))
+begin
+	if(valid_int_id(p_id_usuario)) then
+		set p_nombre = trim_and_lower(p_nombre);
+		set p_nombre_usuario = trim_and_lower(p_nombre_usuario);
+    
+		if (is_empty(p_nombre_usuario)) then
+			UPDATE tb_usuario
+			set nombre = p_nombre
+			where id = p_id_usuario
+            and eliminado = false;
+        else 
+			UPDATE tb_usuario
+			set nombre = p_nombre,
+				nombre_usuario = p_nombre_usuario
+			where id = p_id_usuario
+			and eliminado = false;
+        end if;
+    end if;
+	
+end $$
+delimiter ;
+*/
+
+/*
+drop procedure if exists `proc_update_password_usuario_by_id`;
+delimiter $$
+create procedure proc_update_password_usuario_by_id(in p_id_usuario bigint,
+													in p_clave varchar(255))
+begin
+	if (is_empty(p_clave) = false and
+		int_valid_id(p_id_usuario)) then
+		update tb_usuario 
+        set clave = p_clave
+        where id = p_id_usuario
+        and eliminado = false;
+    end if;
+end $$
+delimiter ;
+*/
+
+/*
+drop procedure if exists `proc_set_usuario_admin_by_id`;
+delimiter $$
+create procedure proc_set_usuario_admin_by_id(in p_id_usuario bigint,
+												in p_id_usuario_admin bigint)
+begin
+	if(valid_int_id(p_id_usuario) and
+		valid_int_id(p_id_usuario_admin) and
+        func_is_user_admin(id_usuario_admin)) then
+		update tb_usuario set admin = true
+        where id = p_id_usuario and eliminado = false;
+    end if;
+end $$
+delimiter ;
+*/
+
 
