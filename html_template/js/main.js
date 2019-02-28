04239ebf93c4321a0757cdc7bb9bb33f226626ac
Angel Hernandez <angel_hernandez998@hotmail.com>
2019-02-27 21:35:37 -0600

    Pantallas de ventas completas, ahora estamos con devoluciones

--- html_template/js/main.js
+++ html_template/js/main.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     M.Sidenav.init(document.querySelectorAll('.sidenav'), []);
-
     M.Collapsible.init(document.querySelectorAll('.collapsible'), []);
+    M.Modal.init(document.querySelectorAll('.modal'), []);
+    M.Dropdown.init(document.querySelectorAll('.dropdown-trigger'), []);
+    M.FormSelect.init(document.querySelectorAll('select'), []);
 });
