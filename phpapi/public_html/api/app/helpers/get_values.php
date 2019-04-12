71dea38b19f5800658dea7ac713ce9a2449f4726
Angel Hernandez <angel_hernandez998@hotmail.com>
2019-04-12 00:34:38 -0600

    Se agrego una funcion para obtener strings random en PHP, se ordeno la base de datos y se agregaron imagenes en public_html para proceder a tener la api en linea

--- phpapi/public_html/api/app/helpers/get_values.php
+++ phpapi/public_html/api/app/helpers/get_values.php
@@ -79,3 +79,13 @@
         } 
         return $json;
     }
+
+    function get_random_str($length = 15) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
