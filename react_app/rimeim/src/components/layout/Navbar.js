176e079f1ccb3f9bb188bc9050eaf4b3fbcbd4b3
Angel Hernandez <angel_hernandez998@hotmail.com>
2019-05-04 13:07:41 -0600

    Rimeim V1

--- react_app/rimeim/src/components/layout/Navbar.js
+++ react_app/rimeim/src/components/layout/Navbar.js
@@ -1,57 +1,57 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-import logo_rimeim from '../../public/img/logo_rimeim.png';
+import logo_rimeim from "../../public/img/logo_rimeim.png";
 
 // Sells
-import NewSellMobile from './mobilemenu/NewSellMobile';
-import NewSellWrapper from './topnav/NewSellWrapper';
-import SellsWrapper from './topnav/SellsWrapper';
-import SearchSellWrapper from './topnav/SearchSellWrapper';
+import NewSellMobile from "./mobilemenu/NewSellMobile";
+import NewSellWrapper from "./topnav/NewSellWrapper";
+import SellsWrapper from "./topnav/SellsWrapper";
+import SearchSellWrapper from "./topnav/SearchSellWrapper";
 
 // Quotations
-import NewQuotationMobile from './mobilemenu/NewQuotationMobile';
-import QuotationsWrapper from './topnav/QuotationsWrapper';
-import SearchQuotationsWrapper from './topnav/SearchQuotationsWrapper';
-import NewQuotationWrapper from './topnav/NewQuotationWrapper';
+import NewQuotationMobile from "./mobilemenu/NewQuotationMobile";
+import QuotationsWrapper from "./topnav/QuotationsWrapper";
+import SearchQuotationsWrapper from "./topnav/SearchQuotationsWrapper";
+import NewQuotationWrapper from "./topnav/NewQuotationWrapper";
 
 // Devolutions
-import DevolutionsWrapper from './topnav/DevolutionsWrapper';
-import SearchDevolutionWrapper from './topnav/SearchDevolutionWrapper';
-import NewDevolutionWrapper from './topnav/NewDevolutionWrapper';
+import DevolutionsWrapper from "./topnav/DevolutionsWrapper";
+import SearchDevolutionWrapper from "./topnav/SearchDevolutionWrapper";
+import NewDevolutionWrapper from "./topnav/NewDevolutionWrapper";
 
 // Orders
-import OrdersMobile from './mobilemenu/OrdersMobile';
-import NewOrderWrapper from './topnav/NewOrderWrapper';
-import OrderWrapper from './topnav/OrderWrapper';
-import OrdersWrapper from './topnav/OrdersWrapper';
-import SearchOrderWrapper from './topnav/SearchOrderWrapper';
+import OrdersMobile from "./mobilemenu/OrdersMobile";
+import NewOrderWrapper from "./topnav/NewOrderWrapper";
+import OrderWrapper from "./topnav/OrderWrapper";
+import OrdersWrapper from "./topnav/OrdersWrapper";
+import SearchOrderWrapper from "./topnav/SearchOrderWrapper";
 
 // Products
-import NewProductMobile from './mobilemenu/NewProductMobile';
-import BrandsMovile from './mobilemenu/BrandsMovile';
-import VehicleTypeMobile from './mobilemenu/VehicleTypeMobile';
-import ProductsWrapper from './topnav/ProductsWrapper';
-import NewProductWrapper from './topnav/NewProductWrapper';
-import SearchProductWrapper from './topnav/SearchProductWrapper';
-import BrandsWrapper from './topnav/BrandsWrapper';
-import VehicleTypeWrapper from './topnav/VehicleTypeWrapper';
+import NewProductMobile from "./mobilemenu/NewProductMobile";
+import BrandsMovile from "./mobilemenu/BrandsMovile";
+import VehicleTypeMobile from "./mobilemenu/VehicleTypeMobile";
+import ProductsWrapper from "./topnav/ProductsWrapper";
+import NewProductWrapper from "./topnav/NewProductWrapper";
+import SearchProductWrapper from "./topnav/SearchProductWrapper";
+import BrandsWrapper from "./topnav/BrandsWrapper";
+import VehicleTypeWrapper from "./topnav/VehicleTypeWrapper";
 
 // Clients
-import ClientsWrapper from './topnav/ClientsWrapper';
-import NewClientWrapper from './topnav/NewClientWrapper';
-import EditClientWrapper from './topnav/EditClientWrapper';
-import SearchClientWrapper from './topnav/SearchClientWrapper';
+import ClientsWrapper from "./topnav/ClientsWrapper";
+import NewClientWrapper from "./topnav/NewClientWrapper";
+import EditClientWrapper from "./topnav/EditClientWrapper";
+import SearchClientWrapper from "./topnav/SearchClientWrapper";
 
 // Provider
-import ProviderMobile from './mobilemenu/ProviderMobile';
-import ProvidersWrapper from './topnav/ProvidersWrapper';
-import ProviderWrapper from './topnav/ProviderWrapper';
-import NewProviderWrapper from './topnav/NewProviderWrapper';
-import EditProviderWrapper from './topnav/EditProviderWrapper';
-import SearchProviderWrapper from './topnav/SearchProviderWrapper';
+import ProviderMobile from "./mobilemenu/ProviderMobile";
+import ProvidersWrapper from "./topnav/ProvidersWrapper";
+import ProviderWrapper from "./topnav/ProviderWrapper";
+import NewProviderWrapper from "./topnav/NewProviderWrapper";
+import EditProviderWrapper from "./topnav/EditProviderWrapper";
+import SearchProviderWrapper from "./topnav/SearchProviderWrapper";
 
 // Navbar Types
 import {
@@ -82,10 +82,10 @@ import {
   NEW_PROVIDER,
   EDIT_PROVIDER,
   SEARCH_PROVIDER
-} from './NavTypes';
+} from "./NavTypes";
 
 // Functions
-import { logoutUser, setCurrentLocal } from '../../actions/UserActions';
+import { logoutUser, setCurrentLocal } from "../../actions/UserActions";
 
 class Navbar extends Component {
   render() {
@@ -262,174 +262,153 @@ class Navbar extends Component {
       <header>
         {MobileMenu && <MobileMenu />}
 
-        <nav className='red lighten-1 top-nav'>
-          <div className='small-container'>{NavWrapper && <NavWrapper />}</div>
+        <nav className="red lighten-1 top-nav">
+          <div className="small-container">{NavWrapper && <NavWrapper />}</div>
         </nav>
 
-        <ul className='sidenav sidenav-fixed' id='nav_sidenav'>
-          <li className='logo'>
-            <a href='#!' className='brand-logo center mt-1'>
-              <img src={logo_rimeim} className='logo-sidenav' alt='' />
+        <ul className="sidenav sidenav-fixed" id="nav_sidenav">
+          <li className="logo">
+            <a href="#!" className="brand-logo center mt-1">
+              <img src={logo_rimeim} className="logo-sidenav" alt="" />
             </a>
-            <div className='divider' />
+            <div className="divider" />
           </li>
-          <li className='no-padding'>
-            <ul className='collapsible collapsible-accordion pb-navbar'>
-              <li className={`bold ${active_sells && 'active'}`}>
-                <a className='collapsible-header' tabIndex='0' href='#!'>
-                  <i className='material-icons'>monetization_on</i>
+          <li className="no-padding">
+            <ul className="collapsible collapsible-accordion pb-navbar">
+              <li className={`bold ${active_sells && "active"}`}>
+                <a className="collapsible-header" tabIndex="0" href="#!">
+                  <i className="material-icons">monetization_on</i>
                   Ventas
                 </a>
-                <div className='collapsible-body'>
+                <div className="collapsible-body">
                   <ul>
                     <li>
-                      <Link to='/nueva_venta'> Nueva </Link>
+                      <Link to="/nueva_venta"> Nueva </Link>
                     </li>
                     <li>
-                      <Link to='/ventas'> Historial </Link>
-                    </li>
-                    <li>
-                      <Link to='/buscar_venta'>Buscar</Link>
+                      <Link to="/ventas"> Historial </Link>
                     </li>
                   </ul>
                 </div>
               </li>
-              <li className={`bold ${active_quotes && 'active'}`}>
-                <a className='collapsible-header' tabIndex='0' href='#!'>
-                  <i className='material-icons'>library_books</i>
+              <li className={`bold ${active_quotes && "active"}`}>
+                <a className="collapsible-header" tabIndex="0" href="#!">
+                  <i className="material-icons">library_books</i>
                   Cotizaciones
                 </a>
-                <div className='collapsible-body'>
+                <div className="collapsible-body">
                   <ul>
                     <li>
-                      <Link to='/nueva_cotizacion'>Nueva</Link>
+                      <Link to="/nueva_cotizacion">Nueva</Link>
                     </li>
                     <li>
-                      <Link to='/cotizaciones'>Historial</Link>
-                    </li>
-                    <li>
-                      <Link to='/buscar_cotizacion'>Buscar</Link>
+                      <Link to="/cotizaciones">Historial</Link>
                     </li>
                   </ul>
                 </div>
               </li>
-              <li className={`bold ${active_devolutions && 'active'}`}>
-                <a className='collapsible-header' tabIndex='0' href='#!'>
-                  <i className='material-icons'>money_off</i>
+              <li className={`bold ${active_devolutions && "active"}`}>
+                <a className="collapsible-header" tabIndex="0" href="#!">
+                  <i className="material-icons">money_off</i>
                   Devoluciones
                 </a>
-                <div className='collapsible-body'>
+                <div className="collapsible-body">
                   <ul>
                     <li>
-                      <Link to='/devoluciones'>Historial</Link>
-                    </li>
-                    <li>
-                      <Link to='/buscar_devolucion'>Buscar</Link>
+                      <Link to="/devoluciones">Historial</Link>
                     </li>
                   </ul>
                 </div>
               </li>
-              <li className={`bold ${active_orders && 'active'}`}>
-                <a className='collapsible-header' tabIndex='0' href='#!'>
-                  <i className='material-icons'>border_color</i>
+              <li className={`bold ${active_orders && "active"}`}>
+                <a className="collapsible-header" tabIndex="0" href="#!">
+                  <i className="material-icons">border_color</i>
                   Pedidos
                 </a>
-                <div className='collapsible-body'>
+                <div className="collapsible-body">
                   <ul>
                     <li>
-                      <Link to='/nuevo_pedido'>Nuevo</Link>
+                      <Link to="/nuevo_pedido">Nuevo</Link>
                     </li>
                     <li>
-                      <Link to='/pedidos'>Pedidos</Link>
-                    </li>
-                    <li>
-                      <Link to='/buscar_pedido'>Buscar</Link>
+                      <Link to="/pedidos">Pedidos</Link>
                     </li>
                   </ul>
                 </div>
               </li>
-              <li className={`bold ${active_products && 'active'}`}>
-                <a className='collapsible-header' tabIndex='0' href='#!'>
-                  <i className='material-icons'>directions_car</i>
+              <li className={`bold ${active_products && "active"}`}>
+                <a className="collapsible-header" tabIndex="0" href="#!">
+                  <i className="material-icons">directions_car</i>
                   Productos
                 </a>
-                <div className='collapsible-body'>
+                <div className="collapsible-body">
                   <ul>
                     <li>
-                      <Link to='/productos'>Inventario</Link>
+                      <Link to="/productos">Inventario</Link>
                     </li>
                     <li>
-                      <Link to='/buscar_producto'>Buscar</Link>
+                      <Link to="/nuevo_producto">Nuevo</Link>
                     </li>
                     <li>
-                      <Link to='/nuevo_producto'>Nuevo</Link>
+                      <Link to="/marcas">Marcas</Link>
                     </li>
                     <li>
-                      <Link to='/marcas'>Marcas</Link>
-                    </li>
-                    <li>
-                      <Link to='/vehiculos'>Tipo de vehiculo</Link>
+                      <Link to="/vehiculos">Tipo de vehiculo</Link>
                     </li>
                   </ul>
                 </div>
               </li>
-              <li className={`bold ${active_clients && 'active'}`}>
-                <a className='collapsible-header' tabIndex='0' href='#!'>
-                  <i className='material-icons'>group</i>
+              <li className={`bold ${active_clients && "active"}`}>
+                <a className="collapsible-header" tabIndex="0" href="#!">
+                  <i className="material-icons">group</i>
                   Clientes
                 </a>
-                <div className='collapsible-body'>
+                <div className="collapsible-body">
                   <ul>
                     <li>
-                      <Link to='/nuevo_cliente'>Nuevo</Link>
+                      <Link to="/nuevo_cliente">Nuevo</Link>
                     </li>
                     <li>
-                      <Link to='/clientes'>Ver todos</Link>
-                    </li>
-                    <li>
-                      <Link to='/buscar_cliente'>Buscar</Link>
+                      <Link to="/clientes">Ver todos</Link>
                     </li>
                   </ul>
                 </div>
               </li>
-              <li className={`bold ${active_providers && 'active'}`}>
-                <a className='collapsible-header' tabIndex='0' href='#!'>
-                  <i className='material-icons'>perm_contact_calendar</i>
+              <li className={`bold ${active_providers && "active"}`}>
+                <a className="collapsible-header" tabIndex="0" href="#!">
+                  <i className="material-icons">perm_contact_calendar</i>
                   Proveedor
                 </a>
-                <div className='collapsible-body'>
+                <div className="collapsible-body">
                   <ul>
                     <li>
-                      <Link to='/nuevo_proveedor'>Nuevo</Link>
+                      <Link to="/nuevo_proveedor">Nuevo</Link>
                     </li>
                     <li>
-                      <Link to='/proveedores'>Ver todos</Link>
-                    </li>
-                    <li>
-                      <Link to='/buscar_proveedor'>Buscar</Link>
+                      <Link to="/proveedores">Ver todos</Link>
                     </li>
                   </ul>
                 </div>
               </li>
-              <div className='divider' />
-              <li className='bold'>
-                <a className='collapsible-header' tabIndex='0' href='#!'>
+              <div className="divider" />
+              <li className="bold">
+                <a className="collapsible-header" tabIndex="0" href="#!">
                   {has_notifications ? (
-                    <i className='material-icons notifications-active'>
+                    <i className="material-icons notifications-active">
                       notifications_active
                     </i>
                   ) : (
-                    <i className='material-icons'>notifications</i>
+                    <i className="material-icons">notifications</i>
                   )}
                   Cuenta
                 </a>
-                <div className='collapsible-body'>
+                <div className="collapsible-body">
                   <ul>
                     <li>
-                      <a href='#!'>
+                      <a href="#!">
                         <i
                           className={`material-icons ${has_notifications &&
-                            'notifications-active-color'}`}
+                            "notifications-active-color"}`}
                         >
                           notifications
                         </i>
@@ -439,30 +418,30 @@ class Navbar extends Component {
                     {admin && (
                       <li>
                         <a
-                          href='#!'
+                          href="#!"
                           onClick={() => {
                             this.props.setCurrentLocal(null);
                           }}
                         >
-                          <i className='material-icons'>compare_arrow</i>
+                          <i className="material-icons">compare_arrow</i>
                           Cambiar de local
                         </a>
                       </li>
                     )}
                     <li>
-                      <Link to='/configuracion'>
-                        <i className='material-icons'>settings</i>
+                      <Link to="/configuracion">
+                        <i className="material-icons">settings</i>
                         Configuracion
                       </Link>
                     </li>
                     <li>
                       <a
-                        href='#!'
+                        href="#!"
                         onClick={() => {
                           this.props.logoutUser();
                         }}
                       >
-                        <i className='material-icons'>exit_to_app</i>
+                        <i className="material-icons">exit_to_app</i>
                         Cerrar sesion
                       </a>
                     </li>
--- react_app/rimeim/src/components/pages/devolutions/Devolutions.js
+++ react_app/rimeim/src/components/pages/devolutions/Devolutions.js
@@ -1,63 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import { DEVOLUTIONS } from "../../layout/NavTypes"
-import Navbar from "../../layout/Navbar"
+import { DEVOLUTIONS } from "../../layout/NavTypes";
+import Navbar from "../../layout/Navbar";
 
 import {
-    configMaterialComponents,
-    removeMaterialComponents
-} from "../../../utils/MaterialFunctions"
+  configMaterialComponents,
+  removeMaterialComponents
+} from "../../../utils/MaterialFunctions";
 
-import DevolutionCard from "../../common/DevolutionCard"
+import DevolutionCard from "../../common/DevolutionCard";
 
 class Devolutions extends Component {
+  state = {
+    devoluciones: []
+  };
 
-    state = {
-        devoluciones: [
-            {
-                id: 1,
-                codigo: "#1256",
-                fecha: "Hoy",
-                cliente: {
-                    nombre: "Marcos"
-                },
-                vendedor: {
-                    id: 12,
-                    nombre: "Magostino"
-                },
-                total: 1122
-            }
-        ]
-    }
+  componentWillMount() {
+    removeMaterialComponents();
+  }
 
-    componentWillMount() {
-        removeMaterialComponents()
-    }
+  componentDidMount() {
+    configMaterialComponents();
+  }
 
-    componentDidMount() {
-        configMaterialComponents()
-    }
+  render() {
+    const { devoluciones } = this.state;
+    return (
+      <React.Fragment>
+        <Navbar navtype={DEVOLUTIONS} />
 
-    render() {
-        const { devoluciones } = this.state
-        return (
-            <React.Fragment>
-                <Navbar navtype={DEVOLUTIONS} />
-
-                <main>
-                    <div className="row">
-                        <div className="col s12">
-                            {devoluciones.map((devolucion, i) => {
-                                return (
-                                    <DevolutionCard devolucion={devolucion} key={devolucion.id} />
-                                )
-                            })}
-                        </div>
-                    </div>
-                </main>
-            </React.Fragment>
-        )
-    }
+        <main>
+          <div className="row">
+            <div className="col s12">
+              {devoluciones.map((devolucion, i) => {
+                return (
+                  <DevolutionCard devolucion={devolucion} key={devolucion.id} />
+                );
+              })}
+            </div>
+          </div>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
-export default Devolutions
+export default Devolutions;
